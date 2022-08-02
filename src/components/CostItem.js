6342096bcfd4d530f043134c26605d534490f713
anatolicheban <107035461+anatolicheban@users.noreply.github.com>
2022-08-02 16:42:06 +0300

    first commit

---
+++ src/components/CostItem.js
@@ -0,0 +1,12 @@
+import './CostItem.css'
+function CostItem() {
+    return (
+        <div>
+            <div>март 2021 12</div>
+            <h2>Холодильник</h2>
+            <div>$999.99</div>
+        </div>
+    );
+}
+
+export default CostItem;
